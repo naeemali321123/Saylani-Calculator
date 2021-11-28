361fee0b57f20e88964251f67ac967d86dd4fda0
naeemali321123 <naeemali321123@gmail.com>
2021-11-28 20:22:15 +0500

    28-11-21

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function one() {
+function sendBtnValue(btnValue) {
+    document.getElementById("currentOperand").innerHTML += btnValue;
     sound();
-    
-
 }
 function sound() {
     var audio = new Audio('../mp3/click.mp3');
     audio.play();
+
 }
