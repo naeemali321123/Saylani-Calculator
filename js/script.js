e9d647b5dca4dff2a2f7e3c10c1f8019ba9480f1
naeemali321123 <naeemali321123@gmail.com>
2021-11-29 00:05:08 +0500

    functions calculator 29-11-21 at home

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
+function sound() {
+    var audio = new Audio('../mp3/click.mp3');
+    audio.play();
+}
+
 function sendBtnValue(btnValue) {
     document.getElementById("currentOperand").innerHTML += btnValue;
     sound();
 }
-function sound() {
-    var audio = new Audio('../mp3/click.mp3');
-    audio.play();
+
+function allClear() {
+    document.getElementById("currentOperand").innerHTML = null;
+    sound();
+}
+
+function plus() {
+    var plusSign = "+";
+    document.getElementById("currentOperand").innerHTML += plusSign;
+    sound();
+}
+
+function minus() {
+    var minusSign = "-";
+    document.getElementById("currentOperand").innerHTML += minusSign;
+    sound();
+}
+
+function multiply() {
+    var multiplySign = "*";
+    document.getElementById("currentOperand").innerHTML += multiplySign;
+    sound();
+}
+
+function devide() {
+    var devideSign = "/";
+    document.getElementById("currentOperand").innerHTML += devideSign;
+    sound();
+}
+
+function point() {
+    var pointSign = ".";
+    document.getElementById("currentOperand").innerHTML += pointSign;
+    sound();
+}
+
+function equal() {
+    var screenData = document.getElementById("currentOperand").innerHTML;
+    document.getElementById("currentOperand").innerHTML = eval(screenData);
+    sound();
 
 }
